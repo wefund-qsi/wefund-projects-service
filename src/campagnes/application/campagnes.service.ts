1774498a91686e3a1c42c916c65afc9e3266cd07
adama <adama.coundoul.etu@univ-lille.com>
2026-03-17 00:52:03 +0100

    feat(story 6): clotûre automatique d'une campagne

--- src/campagnes/application/campagnes.service.ts
+++ src/campagnes/application/campagnes.service.ts
@@ -1,14 +1,18 @@
-import { Injectable, NotFoundException, BadRequestException, UnauthorizedException } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException, UnauthorizedException ,Logger} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { LessThanOrEqual, Repository } from 'typeorm';
 import { CampagneEntity, StatutCampagne } from '../domain/campagne.entity';
 import { CreateCampagneDto } from '../dto/create-campagne.dto';
 import { ProjectsService } from '../../projects/application/projects.service';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 const MOCK_PORTEUR_ID = "cm9x8y7z6w5v4u3t2s1r0q";
 
 @Injectable()
 export class CampagnesService {
+
+  private readonly logger = new Logger(CampagnesService.name);
+
   constructor(
     @InjectRepository(CampagneEntity)
     private readonly campagneRepository: Repository<CampagneEntity>,
@@ -39,4 +43,48 @@ export class CampagnesService {
 
     return await this.campagneRepository.save(campagne);
   }
+
+  @Cron(CronExpression.EVERY_10_SECONDS) 
+  async cloturerCampagnesTerminees(): Promise<void> {
+    this.logger.log('Vérification des campagnes arrivées à échéance...');
+
+    const currentDate = new Date();
+
+    const campagnesACloturer = await this.campagneRepository.find({
+      where: {
+        statut: StatutCampagne.ACTIVE,
+        dateFin: LessThanOrEqual(currentDate), 
+      },
+    });
+
+    if (campagnesACloturer.length === 0) {
+      this.logger.log('Aucune campagne à clôturer pour le moment.');
+      return;
+    }
+
+    this.logger.log(`${campagnesACloturer.length} campagne(s) à clôturer trouvée(s).`);
+
+    for (const campagne of campagnesACloturer) {
+      try {
+        const objectifAtteint = Number(campagne.montantCollecte) >= Number(campagne.objectif);
+
+        if (objectifAtteint) {
+          campagne.statut = StatutCampagne.REUSSIE;
+        } else {
+          campagne.statut = StatutCampagne.ECHOUEE;
+        }
+
+        await this.campagneRepository.save(campagne);
+        
+        this.logger.log(`Campagne [${campagne.id}] clôturée avec succès. Nouveau statut : ${campagne.statut}`);
+
+       
+
+      } catch (error) {
+        
+        this.logger.error(`Erreur lors de la clôture de la campagne [${campagne.id}]`, error.stack);
+      }
+    }
+  }
+
 }
